ccb13431ec16b7a9731e4c47a53d51e1f89c2981
CATHERINE ISABEL USME GOMEZ <catherineusme301254@correo.itm.edu.co>
2025-06-03 00:37:42 -0500

    Html dueño consultart todos y consultarXcedula

--- Paginas/Scripts/Dueno.js
+++ Paginas/Scripts/Dueno.js
@@ -1,7 +1,7 @@
 ﻿var UrlBase = "http://localhost:44302/";
 
 jQuery(function () {
-    //Registrar los botones para responder al evento click
+    //Registrar los botones para responder al evento click 
     $("#dvMenu").load("../Paginas/Menu.html")
     LlenarTablaDuenos();
 });
