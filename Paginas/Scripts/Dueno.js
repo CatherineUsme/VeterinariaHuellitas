bf05bb39107a36f2f9f100095e7387ec797891d9
CATHERINE ISABEL USME GOMEZ <catherineusme301254@correo.itm.edu.co>
2025-06-03 00:25:06 -0500

    Html Dueños

--- Paginas/Scripts/Dueno.js
+++ Paginas/Scripts/Dueno.js
@@ -4,11 +4,33 @@ jQuery(function () {
     //Registrar los botones para responder al evento click
     $("#dvMenu").load("../Paginas/Menu.html")
     LlenarTablaDuenos();
-
 });
-
 function LlenarTablaDuenos() {
     let URL = UrlBase + "api/duenos/ConsultarTodos";
-    LlenarTablaXServicios(URL, "#tblDuenos");
+    LlenarTablaXServiciosAuth(URL, "#tblDuenos");
+}
+
+function Consultar() {
+    let cedula = $("#txtDocumento").val();
+    let URL = UrlBase + "api/duenos/ConsultarXCedula?cedula=" + cedula;
+    const dueno = await ConsultarServicioAuth(URL);
+    if (dueno == null || empleado == undefined) {
+        $("#dvMensaje").removeClass("alert alert-success");
+        $("#dvMensaje").addClass("alert alert-danger");
+        $("#dvMensaje").html("El dueño no existe");
+    }
+    else {
+        $("#txtDocumento").val(dueno.cedula);
+        $("#txtNombre").val(dueno.Nombre);
+        $("#txtApellido").val(dueno.apellido);
+        $("#txtEmail").val(dueno.email);
+        $("#txtTelefono").val(dueno.telefono);
+        $("#txtDireccion").val(dueno.direccion);
+        $("#txtFechaRegistro").val(dueno.fecha_registro.split('T')[0]);
+        $("#chkActivo").prop("checked", dueno.activo);)
+
+    }
+    
+}
 
 }
