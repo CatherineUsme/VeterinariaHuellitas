2e18458071c974905d57c837f2aea14347c1bba9
CATHERINE ISABEL USME GOMEZ <catherineusme301254@correo.itm.edu.co>
2025-06-03 00:38:29 -0500

    no message

--- Paginas/Scripts/Dueno.js
+++ Paginas/Scripts/Dueno.js
@@ -10,27 +10,40 @@ function LlenarTablaDuenos() {
     LlenarTablaXServiciosAuth(URL, "#tblDuenos");
 }
 
-function Consultar() {
+async function Consultar() {
     let cedula = $("#txtDocumento").val();
     let URL = UrlBase + "api/duenos/ConsultarXCedula?cedula=" + cedula;
     const dueno = await ConsultarServicioAuth(URL);
-    if (dueno == null || empleado == undefined) {
+    if (dueno == null || dueno == undefined) {
         $("#dvMensaje").removeClass("alert alert-success");
         $("#dvMensaje").addClass("alert alert-danger");
         $("#dvMensaje").html("El dueño no existe");
     }
     else {
         $("#txtDocumento").val(dueno.cedula);
-        $("#txtNombre").val(dueno.Nombre);
-        $("#txtApellido").val(dueno.apellido);
+        $("#txtNombre").val(dueno.nombre);
+        $("#txtApellidos").val(dueno.apellido);
         $("#txtEmail").val(dueno.email);
         $("#txtTelefono").val(dueno.telefono);
         $("#txtDireccion").val(dueno.direccion);
-        $("#txtFechaRegistro").val(dueno.fecha_registro.split('T')[0]);
-        $("#chkActivo").prop("checked", dueno.activo);)
+        $("#txtId").val(dueno.id_dueno); 
+        $("#txtFechaCreacion").val(dueno.fecha_registro.split('T')[0]);
+        $("#chkActivo").prop("checked", dueno.activo);
 
     }
     
 }
 
+class dueno {
+    constructor(id_dueno, nombre, apellido, cedula, email, telefono, direccion, fecha_registro, activo) {
+        this.id_dueno = id_dueno;
+        this.Nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.email = email;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.fecha_registro = fecha_registro;
+        this.activo = activo;
+    }
 }
